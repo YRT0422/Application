6efd52f1f1b92d51e35472ba8b1d2fcd9354a962
YRT0422 <runting.yang@mail.utoronto.ca>
2020-06-04 15:31:50 -0400

    Linked start window with new project window.

--- src/app/startController.java
+++ src/app/startController.java
@@ -1,4 +1,28 @@
 package app;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class startController {
+    public Button newProjectButton;
+
+
+    @FXML
+    private void newProjectButtonOnClicked(){
+        try {
+            Parent newProjectRoot = FXMLLoader.load(getClass().getResource("newProject.fxml"));
+            Stage newProjectStage = new Stage();
+            newProjectStage.setTitle("新建项目");
+            newProjectStage.setScene(new Scene(newProjectRoot, 600, 400));
+            newProjectStage.show();
+            Stage stage = (Stage) newProjectButton.getScene().getWindow();
+            stage.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
