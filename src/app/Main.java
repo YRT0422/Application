6da9db0f0eb49bca19a26ad884e0909bdda1cad3
YRT0422 <runting.yang@mail.utoronto.ca>
2020-06-08 16:03:12 -0400

    Updated UI designs.

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,14 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
-//        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("招标项目管理");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
--- src/app/startController.java
+++ src/app/startController.java
@@ -1,28 +1,43 @@
 package app;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class startController {
-    public Button newProjectButton;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class startController implements Initializable {
+    public Button manageButton;
+    @FXML
+    private ImageView imageView;
 
     @FXML
-    private void newProjectButtonOnClicked(){
+    private void manageButtonOnClicked(){
         try {
-            Parent newProjectRoot = FXMLLoader.load(getClass().getResource("newProject.fxml"));
+            Parent newProjectRoot = FXMLLoader.load(getClass().getResource("subproject.fxml"));
             Stage newProjectStage = new Stage();
             newProjectStage.setTitle("新建项目");
             newProjectStage.setScene(new Scene(newProjectRoot, 600, 400));
             newProjectStage.show();
-            Stage stage = (Stage) newProjectButton.getScene().getWindow();
+            Stage stage = (Stage) manageButton.getScene().getWindow();
             stage.close();
         } catch (Exception e){
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File file = new File("src/app/drawable/logo.png");
+        Image image = new Image(file.toURI().toString());
+        imageView.setImage(image);
     }
 }
